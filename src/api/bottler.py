df0a8ec2141295e94da6ec5a7ddad0ae737ed8b4
Wander03 <adkerr2001@gmail.com>
2023-09-12 17:33:09 -0700

    test 3

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -3,7 +3,7 @@ from enum import Enum
 from pydantic import BaseModel
 from src.api import auth
 import sqlalchemy as sa
-from src.api import database as db
+from src import database as db
 
 
 router = APIRouter(
